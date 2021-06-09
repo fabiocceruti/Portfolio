38fb442f899a87a9bd6ec610e166ebe1234f317b
Fabio Ceruti <fabiocceruti10@gmail.com>
2021-06-09 10:52:59 -0300

    del file

--- db_starter.py
+++
@@ -1,9 +0,0 @@
-from Classes import RecomendadorYoutube
-
-if __name__ == '__main__':
-
-    recomendador = RecomendadorYoutube(50,30)
-
-    recomendador.Create_Conection()
-
-    recomendador.Update_Data()
